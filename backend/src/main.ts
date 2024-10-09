1c0615b24dce6991c833834a980a3957617d7180
Артем Садреев <artemsadreev@MacBook-Air-Artem.local>
2024-10-10 00:17:16 +0500

    fix cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,7 +5,24 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({
+    origin: 'https://pvb-university.com',
+    methods: 'GET,POST,PUT,DELETE,OPTIONS',
+    credentials: true,
+  });
+
+  app.use((req, res, next) => {
+    if (req.method === 'OPTIONS') {
+      res.header('Access-Control-Allow-Origin', 'https://pvb-university.com');
+      res.header(
+        'Access-Control-Allow-Methods',
+        'GET, POST, PUT, DELETE, OPTIONS',
+      );
+      res.header('Access-Control-Allow-Headers', 'Authorization, Content-Type');
+      return res.status(204).send();
+    }
+    next();
+  });
   app.useLogger(app.get(WINSTON_MODULE_NEST_PROVIDER));
   app.useGlobalPipes(new ValidationPipe({ transform: true, whitelist: true }));
   await app.listen(process.env.PORT || 3000);
