cfd6bf37af77d98e0372404b1c01ce848bb46402
Артем Садреев <artemsadreev@MacBook-Air-Artem.local>
2024-10-09 18:14:33 +0500

    cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,7 +5,11 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({
+    origin: 'https://pvb-university.com', // Разрешенный источник
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Разрешенные методы
+    credentials: true, // Разрешить отправку куки и заголовков авторизации
+  });
   app.useLogger(app.get(WINSTON_MODULE_NEST_PROVIDER));
   app.useGlobalPipes(new ValidationPipe({ transform: true, whitelist: true }));
   await app.listen(process.env.PORT || 3000);
